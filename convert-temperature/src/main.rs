61d01abfdbe3efe44d302eacdafc0860cf024d66
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-06-29 17:23:29 +0100

    Temperature converter function (#5)

---
+++ convert-temperature/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let celsius_temp = 23.0;
+    let fahrenheit_temp = celsius_to_fahrenheit(celsius_temp);
+
+    assert_eq!(fahrenheit_temp, 73.4);
+    println!("Test passed!");
+}
+
+fn celsius_to_fahrenheit(celsius_temp :f64) -> f64 {
+    1.8 * celsius_temp + 32.0
+}
