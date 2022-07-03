9cea800636a702d92a904da4fb12d99d01104044
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-07-03 20:13:23 +0100

    Implement timeout
    TODO: cancel input task

--- quiz/src/main.rs
+++ quiz/src/main.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 use csv;
 
 use tokio::{self, task, time};
-use tokio::sync::{oneshot, mpsc};
+use tokio::sync::mpsc;
 use tokio::time::Duration;
 
 /// Simple program to quiz user, questions and answers provided via a CSV file
@@ -23,20 +23,12 @@ struct Args {
 
 type Record = (String, String);
 
-async fn wait(time_out: u64, sender: oneshot::Sender<bool>) {
+async fn wait(time_out: u64, sender: mpsc::Sender<()>) {
     time::sleep(Duration::from_millis(time_out * 1000)).await;
-    sender.send(true);
+    sender.send(()).await.expect("send timeout");
 }
 
-async fn get_answer(sender: oneshot::Sender<String>) {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-
-    let answer = input.trim();
-    sender.send(answer.to_string());
-}
-
-async fn quiz(problems: Vec<Record>, sender: mpsc::Sender<bool>) {
+async fn quiz(problems: Vec<Record>, sender: mpsc::Sender<i32>) {
     for (i, problem) in problems.iter().enumerate() {
         println!("Problem {}: {}", i+1, problem.0);
         io::stdout().flush().unwrap();
@@ -46,7 +38,9 @@ async fn quiz(problems: Vec<Record>, sender: mpsc::Sender<bool>) {
 
         let answer = input.trim();
         if problem.1.trim() == answer {
-            sender.send(true);
+            sender.send(1).await.expect("answer matched");
+        } else {
+            sender.send(0).await.expect("answer not matched");
         }
     }
 }
@@ -67,45 +61,30 @@ async fn main() {
         problems.push(record)
     }
 
-    let (tx, rx) = mpsc::channel::<bool>(1);
-    let (tx2, rx2) = oneshot::channel::<bool>();
+    let total_problems = problems.len();
 
-    task::spawn(quiz(problems.clone(), tx));
+    let (tx, mut rx) = mpsc::channel::<i32>(1);
+    let (tx2, mut rx2) = mpsc::channel(1);
+
+    task::spawn(quiz(problems, tx));
     task::spawn(wait(args.time_limit, tx2));
 
     let mut correct = 0;
-    for (i, problem) in problems.iter().enumerate() {
-        println!("Problem {}: {}", i+1, problem.0);
-        io::stdout().flush().unwrap();
-
-        let (tx3, rx3) = oneshot::channel::<String>();
-        task::spawn(get_answer(tx3));
-
+    let mut count = 0;
+    loop {
         tokio::select! {
-            answer = rx3 => {
-                let unwraped_answer = answer.unwrap();
-                if unwraped_answer == problem.1 {
-                    correct += 1;
+            r = rx.recv() => {
+                correct += r.unwrap();
+                count += 1;
+                if count == total_problems {
+                    break;
                 }
             }
-            _ = rx2 => {
+            _ = rx2.recv() => {
                 break;
             }
         }
     }
     
-    // for (i, problem) in problems.iter().enumerate() {
-    //     println!("Problem {}: {}", i+1, problem.0);
-    //     io::stdout().flush().unwrap();
-
-    //     let mut input = String::new();
-    //     io::stdin().read_line(&mut input).expect("Failed to read line");
-
-    //     let answer = input.trim();
-    //     if problem.1.trim() == answer {
-    //         correct += 1;
-    //     }
-    // }
-
-    println!("You have scored {} out of {}.", correct, problems.len())
+    println!("You have scored {} out of {}.", correct, total_problems)
 }
