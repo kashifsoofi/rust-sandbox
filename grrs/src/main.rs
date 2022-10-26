fe89d3ce80cf058fdc7a6dda41eade34cecbae6f
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-10-26 22:28:24 +0100

    Refactor for unit test, add blackbox tests

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,5 +1,6 @@
-use std::io::{BufRead, BufReader};
+use std::io::{Read, BufRead, BufReader};
 use std::fs::File;
+use anyhow::{Context, Result};
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -11,16 +12,31 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    let file = File::open(args.path).expect("could not read file");
+    let file = File::open(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
     let reader = BufReader::new(file);
 
+    find_matches(reader, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
+
+fn find_matches<R: Read>(reader: BufReader<R>, pattern: &str, mut writer: impl std::io::Write) {
     for wrapped_line in reader.lines() {
         let line = wrapped_line.unwrap();
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
         }
     }
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    let reader = BufReader::new("lorem ipsum\ndolor sit amet".as_bytes());
+    find_matches(reader, "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
